cad1e815e1802fbdbd194dd119baa44484e7bde7
John-P-Foster <johnpete.foster@gmail.com>
2024-08-27 15:28:18 -0400

    Updating UI

--- src/app/ingredients/new-ingredient/new-ingredient.component.ts
+++ src/app/ingredients/new-ingredient/new-ingredient.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, Output, EventEmitter, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { AppService } from '../../app.service';
 import { CustomDialogComponent } from '../../shared/custom-dialog/custom-dialog.component';
 import { Ingredient } from '../../app.models';
@@ -16,11 +17,28 @@ export class NewIngredientComponent {
   @Output() close = new EventEmitter();
 
   private appService = inject(AppService);
+  private ingredientsSub!: Subscription; 
+
   enteredName = 'Name of New Ingredient';
+  ingredients: Ingredient[]=[]
+
+  ngOnInit(){
+    this.appService.getIngredients();
+    this.ingredientsSub = this.appService.getIngredientsUpdateListner()
+      .subscribe((ingredients: Ingredient[]) =>{
+        this.ingredients = ingredients;
+      });
+      this.appService.firstCallIngreients(); 
+  }
+
 
   onCancel(){
     let e = false;
     this.close.emit(e);
+  }
+
+  deleteIngredient(ingredient: Ingredient){
+    this.appService.deleteIngredient(ingredient._id);
   }
 
   onSubmit(){
